5d6506c1a825bb9a9641e98a3fa1ae033860153f
Shreyans Pathak <ishon19@users.noreply.github.com>
2019-06-04 13:49:53 +0530

    Added grid component
    Grid component added for horizontal list of card/paper

---
+++ src/components/GridCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import {
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button
+} from "@material-ui/core";
+
+const style = makeStyles({
+  card: {
+    maxWidth: 500,
+    marginBottom: 20
+  },
+  media: {
+    height: 150
+  }
+});
